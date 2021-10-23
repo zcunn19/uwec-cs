870b8d8579dd288a30a8c3e601b5c433e52e7579
zcunn19 <cunningham.zachary19@gmail.com>
2021-10-23 15:30:49 -0500

    Yuh

--- Class Exercises/Ex5/Ex5.cpp
+++ Class Exercises/Ex5/Ex5.cpp
@@ -1,52 +1,48 @@
 #include <iostream>
 using namespace std;
- 
-// Function to rearrange the array in given way.
-void rearrangeEvenAndOdd(int arr[], int n){
+
+void rearrangeEvenAndOdd(int arr[], int n)
+{
     int j = -1;
- 
-    // Quick sort method
-    for (int i = 0; i < n; i++) {
- 
-        // If array of element
-        // is odd then swap
-        if (arr[i] % 2 == 0) {
- 
-            // increment j by one
+
+    for (int i = 0; i < n; i++)
+    {
+
+        // If element is odd then swap
+        if (arr[i] % 2 == 0)
+        {
             j++;
- 
             // swap the element
             swap(arr[i], arr[j]);
         }
     }
 }
 
+int main()
+{
 
-int main() {
-
-    int size;
-    int *sip;
+    int size, *sip;
 
     sip = &size;
-    
+
     cout << "enter size of array: ";
     cin >> size;
-    
+
     int data[size]; //declare array of data
     int dat;
 
     //fill array
-    for(int i = 0; i < size; i++){ 
-                  cout << "enter data: ";
-                  cin >> dat;
-                  data[i] = dat;}
+    for (int i = 0; i < size; i++)
+    {
+        cout << "enter data: ";
+        cin >> dat;
+        data[i] = dat;
+    }
 
+    int n = *(&data + 1) - data;
 
-    //int arr[] = {12, 34, 45, 9, 8, 90, 3};
-    int n = sizeof(data) / sizeof(data[0]);
- 
     rearrangeEvenAndOdd(data, n);
- 
+
     for (int i = 0; i < n; i++)
         cout << data[i] << " ";
 
