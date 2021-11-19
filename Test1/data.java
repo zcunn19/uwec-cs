5679e9c3a3c3b69404a445916259cf0fbb8addf4
zcunn19 <cunningham.zachary19@gmail.com>
2021-11-19 13:16:08 -0600

    m

---
+++ Test1/data.java
@@ -0,0 +1,29 @@
+package Test1;
+
+public class data {
+
+    private int age;
+    private String name;
+
+    public data(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+}
---
+++ Test1/driver.java
@@ -0,0 +1,12 @@
+package Test1;
+
+public class driver extends data {
+
+    public static void main(String[] args) {
+
+        data test = new data(10, "Name");
+
+        System.out.println(test.getName());
+        System.out.println(test.getAge());
+    }
+}
