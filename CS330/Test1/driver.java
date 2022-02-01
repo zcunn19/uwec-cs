7a72111c510e12b2439bc09428429fa177b40f4a
zcunn19 <cunningham.zachary19@gmail.com>
2022-02-01 14:50:00 -0600

    yes

--- CS330/Test1/driver.java
+++ CS330/Test1/driver.java
@@ -2,6 +2,11 @@ package Test1;
 
 public class driver extends data {
 
+    public driver(int age, String name) {
+        super(age, name);
+        //TODO Auto-generated constructor stub
+    }
+
     public static void main(String[] args) {
 
         data test = new data(10, "Name");
